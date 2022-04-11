9f9d8a26a200e2d61e003d9e1402637257016681
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-04-11 14:03:47 +0200

    fetch edit

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const BaseUrl = 'https://codingthecurbs.api.fdnd.nl/v1/smartzone'
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
+
 
 app.use(express.static('public'))
-app.use('/scripts',express.static('scripts'))
 // Tamplet engine or rendering engine is ejs
 app.set('view engine','ejs')
+app.set('views','viewes')
+
 // Routs
+
+// method GET
+app.get('/',(request, response) =>{
+
+    fetch(BaseUrl)
+    .then(function(JsonData) {
+        response.render('pages/home', {
+            title:'Smart Zones',
+            home: JsonData.data
+        })
+    })
+})
+
+
 
 app.get('/', (request,response) =>{
     console.log('hall world')
-    response.render('hallo world')
+    response.render('pages/home',{
+        title:'Home'
+    })
 })
 
 app.listen(port, function(){
