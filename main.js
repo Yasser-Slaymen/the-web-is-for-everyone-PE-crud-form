4f057a834353df4e9bd4d0e686fe01adaf94e994
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-04-13 00:22:18 +0200

    method post edit

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const { application } = require('express')
 // const port = process.env.PORT || 3000
 const BaseUrl = 'https://codingthecurbs.api.fdnd.nl/v1/smartzone'
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
+// Create application/form-urlen coded parser
+const urlencodedParser = bodyParser.urlencoded({extended:false})
 
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}));
 // Tamplet engine or rendering engine is ejs
 app.set('view engine','ejs')
 app.set('views','./viewes')
@@ -16,7 +21,7 @@ app.set('views','./viewes')
 
 app.get('/',(request, response) =>{
 
-    fetchJson('https://codingthecurbs.api.fdnd.nl/v1/smartzone').then(function (jsonData) {
+    fetchJson(BaseUrl).then( (jsonData) => {
       response.render('pages/home', {
         title: 'Dit is zones pagina',
         smartzones: jsonData.data,
@@ -25,7 +30,7 @@ app.get('/',(request, response) =>{
 
     
 })
-
+// page for testing
 app.get('/zones', (req, res) => {
     fetchJson('https://codingthecurbs.api.fdnd.nl/v1/smartzone').then(function (jsonData) {
       console.log(jsonData)
@@ -36,6 +41,26 @@ app.get('/zones', (req, res) => {
     })
   })
 
+// Methode:post
+app.post('/add', urlencodedParser, (request, response)=>{
+  const postData = {
+    method:'POST',
+    body:JSON.stringify(request.body),
+    headers:{'Content-Type':'application/json'}
+  }
+  
+  fetchJson(BaseUrl, postData).then(function () {
+    response.render('pages/add',{
+      title:'add new smartzone'
+    })
+  })
+})
+//  renderen  page put in de link
+app.get('/add',(request,response) => {
+    response.render('pages/add', {
+        title: 'edite',
+    })
+})
 
 app.set('port', process.env.PORT ||3000 )
 
@@ -43,11 +68,18 @@ const server = app.listen(app.get('port'), () => {
   console.log(`Application started on port: ${app.get('port')}`)
 })
 
-async function fetchJson(url) {
-    return await fetch(url)
-      .then((response) => response.json())
-      .catch((error) => error)
-  }
+
+async function fetchJson(BaseUrl, postData = {}) {
+  return await fetch(BaseUrl, postData)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
+// for get method
+// async function fetchJson(url) {
+//     return await fetch(url)
+//       .then((response) => response.json())
+//       .catch((error) => error)
+//   }
 // app.listen(port, function(){
 //     console.log(`listening to port ${port}`)
 // })
