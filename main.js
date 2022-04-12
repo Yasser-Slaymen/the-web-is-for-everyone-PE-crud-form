e5fddfe21401913748ed0089d4100ea0e79cac95
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-04-12 13:40:13 +0200

    renderen edit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
 const BaseUrl = 'https://codingthecurbs.api.fdnd.nl/v1/smartzone'
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
@@ -8,31 +8,46 @@ const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fet
 app.use(express.static('public'))
 // Tamplet engine or rendering engine is ejs
 app.set('view engine','ejs')
-app.set('views','viewes')
+app.set('views','./viewes')
 
 // Routs
 
 // method GET
+
 app.get('/',(request, response) =>{
 
-    fetch(BaseUrl)
-    .then(function(JsonData) {
-        response.render('pages/home', {
-            title:'Smart Zones',
-            home: JsonData.data
-        })
+    fetchJson('https://codingthecurbs.api.fdnd.nl/v1/smartzone').then(function (jsonData) {
+      response.render('pages/home', {
+        title: 'Dit is zones pagina',
+        smartzones: jsonData.data,
+      })
     })
+
+    
 })
 
-
-
-app.get('/', (request,response) =>{
-    console.log('hall world')
-    response.render('pages/home',{
-        title:'Home'
+app.get('/zones', (req, res) => {
+    fetchJson('https://codingthecurbs.api.fdnd.nl/v1/smartzone').then(function (jsonData) {
+      console.log(jsonData)
+      res.render('pages/zones', {
+        title: 'Dit is zones pagina',
+        zones: jsonData.data,
+      })
     })
+  })
+
+
+app.set('port', process.env.PORT ||3000 )
+
+const server = app.listen(app.get('port'), () => {
+  console.log(`Application started on port: ${app.get('port')}`)
 })
 
-app.listen(port, function(){
-    console.log(`listening to port ${port}`)
-})
+async function fetchJson(url) {
+    return await fetch(url)
+      .then((response) => response.json())
+      .catch((error) => error)
+  }
+// app.listen(port, function(){
+//     console.log(`listening to port ${port}`)
+// })
